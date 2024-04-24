f3ffc6d1374e6f0a5b82fdfc20a81de7c42339a1
Diiego <dieguitokmino@gmail.com>
2024-04-23 21:53:10 -0500

    se termina el modulo de vaciones

--- src/components/Vaciones.jsx
+++ src/components/Vaciones.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
-import VacacionesSalario from './VacacionesSalario';
-import Independiente from './Independiente';
+import React, { useState, useEffect } from 'react';
 
 function Vacaciones() {
-  const [currentPage, setCurrentPage] = useState('Dependiente');
+  const [salario, setSalario] = useState('');
+  const [diasTrabajados, setDiasTrabajados] = useState('');
+  const [vacaciones, setVacaciones] = useState(0);
 
-  const handlePageChange = (e) => {
-    setCurrentPage(e.target.value);
+  const salarioCambio = (e) => {
+    setSalario(e.target.value);
   };
+
+  const diasTrabajadosSemestre = (e) => {
+    setDiasTrabajados(e.target.value);
+  };
+
+  const calcularVacaciones = (e) => {
+    e.preventDefault();
+    const vacaciones = (salario * diasTrabajados) / 720;
+    setVacaciones(Math.round(vacaciones)); 
+  };
+  useEffect(() => {
+    document.title = 'Vacaciones';
+  }, []);
 
   return (
     <>
-      <select value={currentPage} onChange={handlePageChange}>
-        <option value="VacacionesSalario">VacacionesSalario</option>
-        <option value="Independiente">Independiente</option>
-      </select>
-
-        {currentPage === 'VacacionesSalario' && <VacacionesSalario />}
-        {currentPage === 'Independiente' && <Independiente />}
-    
+      <div className="fondo-boroso"></div>
+      <div className="contenedor">
+        <h1>Calcular Vacaciones</h1>
+        <div>
+          <label>Ingrese su salario Base: </label>
+          <input type="number" value={salario} onChange={salarioCambio} />
+        </div>
+        <div>
+          <label>Ingrese los días trabajados: </label>
+          <input type="number" value={diasTrabajados} onChange={diasTrabajadosSemestre} />
+        </div>
+        <button className="boton" onClick={calcularVacaciones}>
+          Calcular
+        </button>
+        <p>Sus vacaciones son de: ${vacaciones}</p>
+      </div>
     </>
   );
 }
